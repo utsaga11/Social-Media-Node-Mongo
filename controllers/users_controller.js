cfa2766f2c10bbea4778d637b9746a1d0ffae344
utsaga11 <utsaga22@gmail.com>
2022-04-06 00:27:58 +0530

    third commit

---
+++ controllers/users_controller.js
@@ -0,0 +1,42 @@
+const User = require("../models/user");
+module.exports.profile = function (req, res) {
+  return res.render("user_profile", {
+    title: "User Profile",
+  });
+};
+
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Sign Up",
+  });
+};
+
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Sign In",
+  });
+};
+
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm - password) {
+    return res.redirect("back");
+  }
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("Error");
+      return;
+    }
+    if (!user) {
+      user.create(req.body, function (err, user) {
+        if (err) {
+          console.log("Error");
+          return;
+        }
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
+module.exports.createSession = function (req, res) {};
--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 // 1
 const express = require("express");
+// 7 cookie
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 8000;
+//4 ejs layouts
+const expressLayouts = require("express-ejs-layouts");
+// 6 database
+const db = require("./config/mongoose");
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
+// 5 statis files
+app.use(express.static("./assets"));
+// extract syles to layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
+app.use(expressLayouts);
+
+// 2 use express router
+app.use("/", require("./routes"));
+//3 set view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 //1
 app.listen(port, function (err) {
